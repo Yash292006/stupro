3185abc00aee8394798b730820d84544351f19ff
Yash292006 <chavanyash1429@gmail.com>
2025-11-11 09:25:56 +0530

    Update stupro.py

--- stupro.py
+++ stupro.py
@@ -10,7 +10,7 @@ if len(sys.argv) == 3:
     mark5 = sys.argv[6]
     print("User provided input values:")
 else:
-    name = "yash
+    name = "yash"
     mark1 = "35"
     mark2 = "35"
     mark3= "35"
